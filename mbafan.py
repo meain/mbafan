6acf52654b37f0cb9927b59147242794b9050a77
Abin Simon <abinsimon10@gmail.com>
2017-04-19 09:07:38 +0530

    Algorithm changes
    Keep it running on high for a bit longer, better cooler than noisy
    Made it liner than step, (maybe make it exponential?)
    Still got to refine the algo.

--- mbafan.py
+++ mbafan.py
@@ -13,22 +13,26 @@ while (1):
     avg_temp = 0
     core1_temp = open(base_dir+'temp8_input').read()
     core2_temp = open(base_dir+'temp9_input').read()
-    avg_temp = int((float(core1_temp) + float(core2_temp))/2)
+    avg_temp = int((float(core1_temp) + float(core2_temp))/2)/1000
 
     # print(avg_temp)
 
     # get the correct fan speed (hopefully correct)
     fan_speed = '2000'
-    if avg_temp > 60000:
-        fan_speed = '6000'
-    elif avg_temp < 60000 and avg_temp > 55000:
-        fan_speed = '4500'
-    elif avg_temp > 50000 and avg_temp < 55000:
-        fan_speed = '3500'
-    elif avg_temp > 45000 and avg_temp < 50000:
-        fan_speed = '2000'
+    if avg_temp > 55:
+        fan_speed = '6200'
+    elif avg_temp < 55 and avg_temp > 45:
+        fan_speed = (avg_temp + 5)*100 + (avg_temp-45)*20
+        if fan_speed <  3000:
+            fan_speed = '3000'
+        elif fan_speed > 6000:
+            fan_speed = '6000'
     else:
-        fan_speed = '1500'
+        fan_speed = '2000'
+
+
+    fan_speed = str(int(fan_speed))
+    # print(fan_speed)
 
     # set fan speed
     open(base_dir+'fan1_output', 'w').write(fan_speed)
